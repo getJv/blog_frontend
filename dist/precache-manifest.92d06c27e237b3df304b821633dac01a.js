deea421b9e9df33d02d8bd0f631da9c8afbbba9f
Jhonatan Morais <jhonatanvinicius@gmail.com>
2019-08-31 23:40:41 -0300

    dist files

---
+++ dist/precache-manifest.92d06c27e237b3df304b821633dac01a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "9c30a9ee0a8bdc75b120",
+    "url": "/js/chunk-vendors.4d02f8a7.js"
+  },
+  {
+    "revision": "93d2189d54d24333db44",
+    "url": "/js/app.8845ac54.js"
+  },
+  {
+    "revision": "70e2a9a5cd2634dddd3f2537492fa339",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9c30a9ee0a8bdc75b120",
+    "url": "/css/chunk-vendors.996aa46e.css"
+  }
+];
